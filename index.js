bbfbd098ed728732d29ced94b25c8a5ad0811ee4
jxa8hst <jean_altidor@homdepot.com>
2019-01-07 14:17:45 -0500

    removed unworking code

--- index.js
+++ index.js
@@ -64,66 +64,6 @@ for (let i = 1; i <= listLength; i++) {
   // TODO save this user
 }
 
-
-async function getNumPages(page) {
-  const NUM_USER_SELECTOR = '#js-pjax-container > div.container > div > div.column.three-fourths.codesearch-results.pr-6 > div.d-flex.flex-justify-between.border-bottom.pb-3 > h3';
-
-  let inner = await page.evaluate((sel) => {
-    let html = document.querySelector(sel).innerHTML;
-    
-    // format is: "69,803 users"
-    return html.replace(',', '').replace('users', '').trim();
-  }, NUM_USER_SELECTOR);
-
-  let numUsers = parseInt(inner);
-
-  console.log('numUsers: ', numUsers);
-
-  /*
-  * GitHub shows 10 resuls per page, so
-  */
-  let numPages = Math.ceil(numUsers / 10);
-  return numPages;
-}
-
-let numPages = await getNumPages(page);
-
-console.log('Numpages: ', numPages);
-
-for (let h = 1; h <= numPages; h++) {
-
-	let pageUrl = searchUrl + '&p=' + h;
-
-	await page.goto(pageUrl);
-
-	let listLength = await page.evaluate((sel) => {
-			return document.getElementsByClassName(sel).length;
-		}, LENGTH_SELECTOR_CLASS);
-
-	for (let i = 1; i <= listLength; i++) {
-		// change the index to the next child
-		let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
-		let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
-
-		let username = await page.evaluate((sel) => {
-				return document.querySelector(sel).getAttribute('href').replace('/', '');
-			}, usernameSelector);
-
-		let email = await page.evaluate((sel) => {
-				let element = document.querySelector(sel);
-				return element? element.innerHTML: null;
-			}, emailSelector);
-
-		// not all users have emails visible
-		if (!email)
-			continue;
-
-		console.log(username, ' -> ', email);
-
-		// TODO save this users
-	}
-}
-
   browser.close();
 }
 
